82bea3438523520b1d028d908888bebb94bf13c3
qiu.zhongyu <zhongyu.qiu@bioduro-sundia.com>
2022-11-22 14:04:29 +0800

    web accessable

--- bin/fetch.js
+++ bin/fetch.js
@@ -1,29 +1,28 @@
-const axios = require('axios');
-
+const axios = require("axios");
 /**
  * Calls the endpoint with authorization bearer token.
- * @param {string} endpoint 
- * @param {string} accessToken 
+ * @param {string} endpoint
+ * @param {string} accessToken
  */
-async function callApi(endpoint, accessToken) {
+async function callApi(endpoint, requestBody, accessToken) {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      Prefer: 'outlook.timezone="Asia/Shanghai"',
+    },
+  };
 
-    const options = {
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    };
+  console.log("request made to web API at: " + new Date().toString());
 
-    console.log('request made to web API at: ' + new Date().toString());
-
-    try {
-        const response = await axios.default.get(endpoint, options);
-        return response.data;
-    } catch (error) {
-        console.log(error)
-        return error;
-    }
-};
+  try {
+    const response = await axios.default.post(endpoint, requestBody, options);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
 
 module.exports = {
-    callApi: callApi
+  callApi: callApi,
 };
--- bin/index.js
+++ bin/index.js
@@ -1,37 +1,66 @@
 #!/usr/bin/env node
 
 // read in env settings
+const Koa = require('koa')
+const app = new Koa()
 require('dotenv').config();
-
+const dayjs = require('dayjs')
 const yargs = require('yargs');
 
 const fetch = require('./fetch');
 const auth = require('./auth');
+// const options = yargs
+//     .usage('Usage: --op <operation_name>')
+//     .option('op', { alias: 'operation', describe: 'operation name', type: 'string', demandOption: true })
+//     .argv;
+const requestBody = {
+    "schedules": [
+        "RS-WUX-D9-Conf1025@bioduro-sundia.com",
+        "RS-WUX-D9-Conf4020@bioduro-sundia.com",
+        "RS-WUX-D9-Conf6018@bioduro-sundia.com",
+        "RS-WUX-D9-Conf6023@bioduro-sundia.com",
+        "RS-WUX-D9-Conf6024@bioduro-sundia.com",
+        "RS-WUX-D9-Conf6025@bioduro-sundia.com",
+        "RS-WUX-D9-Conf6026@bioduro-sundia.com",
+        "RS-WUX-D9-Conf6027-A@bioduro-sundia.com",
+        "RS-WUX-D9-Conf6027-B@bioduro-sundia.com"
+    ],
+    "startTime": {
+        "dateTime": dayjs().format(),
+        "timeZone": "Asia/Shanghai"
+    },
+    "endTime": {
+        "dateTime": dayjs().add(2,"day").format(),
+        "timeZone": "Asia/Shanghai"
+    },
+    "availabilityViewInterval": 60
+}
+// async function main() {
+//     console.log(`You have selected: ${options.op}`);
 
-const options = yargs
-    .usage('Usage: --op <operation_name>')
-    .option('op', { alias: 'operation', describe: 'operation name', type: 'string', demandOption: true })
-    .argv;
+//     switch (yargs.argv['op']) {
+//         case 'getUsers':
 
-async function main() {
-    console.log(`You have selected: ${options.op}`);
+//             try {
+//                 const authResponse = await auth.getToken(auth.tokenRequest);
+//                 const users = await fetch.callApi(auth.apiConfig.uri,requestBody, authResponse.accessToken);
+//                 console.dir(users,{depth:null});
+//             } catch (error) {
+//                 console.log(error);
+//             }
 
-    switch (yargs.argv['op']) {
-        case 'getUsers':
+//             break;
+//         default:
+//             console.log('Select a Graph operation first');
+//             break;
+//     }
+// };
+app.use(async ctx => {
+    const authResponse = await auth.getToken(auth.tokenRequest);
+    const schedules = await fetch.callApi(auth.apiConfig.uri,requestBody,authResponse.accessToken)
+    ctx.body = schedules
+})
 
-            try {
-                const authResponse = await auth.getToken(auth.tokenRequest);
-                const users = await fetch.callApi(auth.apiConfig.uri, authResponse.accessToken);
-                console.log(users);
-            } catch (error) {
-                console.log(error);
-            }
-
-            break;
-        default:
-            console.log('Select a Graph operation first');
-            break;
-    }
-};
-
-main();
+app.listen(4000)
+console.log("start-quick is starting at port 4000");
+// main();
